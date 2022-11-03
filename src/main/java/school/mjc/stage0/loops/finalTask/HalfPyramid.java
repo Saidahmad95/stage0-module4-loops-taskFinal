4a83aae265b612cef94887b8a57b22ae21c80793
Saidahmad95 <murtazoyev00@gmail.com>
2022-11-03 20:54:39 +0500

    first commit

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i=0; i<cathetusLength; i++) {
+            for(int j=(cathetusLength-i)-2; j>=0; j--)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++)
+            {
+                // printing stars
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
